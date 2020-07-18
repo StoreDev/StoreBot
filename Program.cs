46d6bf56cfc3c4c3a9cdf65cc806dc0335886e89
TitleOS <titleos@blucrystalstudios.onmicrosoft.com>
2020-07-18 18:16:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using System;
+using System.Collections;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace StoreBot
+{
+    public class Program
+    {
+
+        public static Task Client_Ready(ReadyEventArgs e)
+        {
+            // let's log the fact that this event occured
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "StoreBot", "Client is ready to process events.", DateTime.Now);
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private static Task Client_GuildAvailable(GuildCreateEventArgs e)
+        {
+            // let's log the name of the guild that was just
+            // sent to our client
+            e.Client.DebugLogger.LogMessage(LogLevel.Info, "StoreBot", $"Guild available: {e.Guild.Name}", DateTime.Now);
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private static Task Client_ClientError(ClientErrorEventArgs e)
+        {
+            // let's log the details of the error that just 
+            // occured in our client
+            e.Client.DebugLogger.LogMessage(LogLevel.Error, "StoreBot", $"Exception occured: {e.Exception.GetType()}: {e.Exception.Message}", DateTime.Now);
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private static Task Commands_CommandExecuted(CommandExecutionEventArgs e)
+        {
+            // let's log the name of the command and user
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Info, "StoreBot", $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'", DateTime.Now);
+
+            // since this method is not async, let's return
+            // a completed task, so that no additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private static async Task Commands_CommandErrored(CommandErrorEventArgs e)
+        {
+            // let's log the error details
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Error, "StoreBot", $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
+
+            // let's check if the error is a result of lack
+            // of required permissions
+            if (e.Exception is ChecksFailedException ex)
+            {
+                // yes, the user lacks required permissions, 
+                // let them know
+
+                var emoji = DiscordEmoji.FromName(e.Context.Client, ":no_entry:");
+
+                // let's wrap the response into an embed
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = "Access denied",
+                    Description = $"{emoji} You do not have the permissions required to execute this command.",
+                    Color = new DiscordColor(0xFF0000) // red
+                };
+                await e.Context.RespondAsync("", embed: embed);
+            }
+        }
+
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine($"StoreBot - {Assembly.GetExecutingAssembly().GetName().Version.ToString()}");
+            DiscordConfiguration defaultconfig = new DiscordConfiguration
+            {
+                Token = "NzE0NDk0NzYwNTUwNjYyMTU0.XxH2Ew.jwWgbOVlqefD9VRavrRZYfibvC4",
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+
+
+            };
+            DiscordClient client = new DiscordClient(defaultconfig);
+            client.Ready += Client_Ready;
+            client.GuildAvailable += Client_GuildAvailable;
+            client.ClientErrored += Client_ClientError;
+            client.UseInteractivity(new InteractivityConfiguration
+            {
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            var ccfg = new CommandsNextConfiguration
+            {
+
+                // enable responding in direct messages
+                EnableDms = true,
+
+                // enable mentioning the bot as a command prefix
+                EnableMentionPrefix = true
+            };
+
+            var Commands = client.UseCommandsNext(ccfg);
+            Commands.CommandErrored += Commands_CommandErrored;
+            Commands.CommandExecuted += Commands_CommandExecuted;
+            Commands.RegisterCommands<StoreCommands>();
+
+            await client.ConnectAsync();
+            await Task.Delay(-1);
+            
+
+
+        }
+
+
+    }
+}
---
+++ StoreCommands.cs
@@ -0,0 +1,121 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using StoreLib.Models;
+using StoreLib.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBot
+{
+    public class StoreCommands : BaseCommandModule
+    {
+        [Command("info"), Description("Returns info about the current StoreBot Instance")]
+        public async Task InfoAsync(CommandContext cct)
+        {
+
+            var embeddialog = new DiscordEmbedBuilder
+            {
+                Title = "StoreBot Info",
+                Color = DiscordColor.Cyan,
+                Description = $"StoreBot Version {Assembly.GetExecutingAssembly().GetName().Version.ToString()}"
+
+
+
+            };
+            await cct.RespondAsync("", false, embeddialog);
+
+        }
+
+        /*
+        [Command("help"), Description("Returns all registered commands and their descriptions.")]
+        public async Task Help(CommandContext cct)
+        {
+            foreach(var command in cct.Client.GetCommandsNext().RegisteredCommands)
+            {
+                StringBuilder message = new StringBuilder();
+                message.AppendLine($"{command.Value.Name} - {command.Value.Description}");
+            }
+        }
+        */
+
+        [Command("packages"), Description("Queries FE3 for packages for the specified ID.")]
+        public async Task PackagesAsync(CommandContext cct, string ID)
+        {
+            DisplayCatalogHandler dcat = DisplayCatalogHandler.ProductionConfig();
+            await dcat.QueryDCATAsync(ID);
+            if (dcat.IsFound)
+            {
+                var productembedded = new DiscordEmbedBuilder()
+                {
+                    Title = dcat.ProductListing.Product.LocalizedProperties[0].ProductTitle,
+                    ImageUrl = dcat.ProductListing.Product.LocalizedProperties[0].Images[0].Uri.Replace("//", "https://"),
+                    Color = DiscordColor.Gold,
+                    Description = dcat.ProductListing.Product.LocalizedProperties[0].ProductDescription
+                };
+                var packages = await dcat.GetPackagesForProductAsync();
+                foreach(PackageInstance package in packages)
+                {
+                    productembedded.AddField(package.PackageMoniker, package.PackageUri.ToString());
+                }
+                productembedded.Build();
+                Debug.WriteLine(dcat.ProductListing.Product.LocalizedProperties[0].Images[0].Uri.Replace("//", "https://"));
+                await cct.RespondAsync("", false, productembedded);
+            }
+            else
+            {
+                await cct.RespondAsync("Product not found.");
+            }
+
+        }
+
+        [Command("advancedquery"), Description("A customizable query that allows the caller to specify the environment and locale.")]
+        public async Task AdvancedQueryAsync(CommandContext cct, string ID, string environment, string localestring)
+        {
+            if(String.IsNullOrEmpty(ID) || String.IsNullOrEmpty(environment) || String.IsNullOrEmpty(localestring))
+            {
+                await cct.RespondAsync("Please supply all required arguments. Example: advancedquery 9wzdncrfj3tj INT US-EN");
+                return;
+            }
+            bool marketresult = Enum.TryParse(localestring.Split('-')[0], out Market market);
+            bool langresult = Enum.TryParse(localestring.Split('-')[1].ToLower(), out Lang lang);
+            if(!marketresult || !langresult)
+            {
+                await cct.RespondAsync($"Invalid Market or Lang specified. Example: US-EN for United States English, you provided Market {localestring.Split('-')[0]} and Language {localestring.Split('-')[1]}");
+                return;
+            }
+            bool environmentresult = Enum.TryParse(environment, out DCatEndpoint env);
+            if (!environmentresult)
+            {
+                await cct.RespondAsync($"Invalid Environment specified. Example: Production for Production, Int for Instance. You provided {environment}");
+                return;
+            }
+            DisplayCatalogHandler customizedhandler = new DisplayCatalogHandler(env, new Locale(market, lang, true));
+            await customizedhandler.QueryDCATAsync(ID);
+            if (customizedhandler.IsFound)
+            {
+                var productembedded = new DiscordEmbedBuilder()
+                {
+                    Title = customizedhandler.ProductListing.Product.LocalizedProperties[0].ProductTitle,
+                    Color = DiscordColor.Gold
+                };
+                productembedded.AddField("Description:", customizedhandler.ProductListing.Product.LocalizedProperties[0].ProductDescription.Substring(0, 1023));
+                productembedded.Build();
+                await cct.RespondAsync("", false, productembedded);
+            }
+            else
+            {
+                await cct.RespondAsync("Product not found.");
+            }
+
+        }
+
+
+    }
+}
